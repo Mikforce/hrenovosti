1b7fab3528021394b37adeab045ed6e3ba6c50ab
Ivan <100131490+Mikforce@users.noreply.github.com>
2023-07-28 20:09:19 +0300

    2
    2

--- main.go
+++ main.go
@@ -25,15 +25,30 @@ func ExampleScrape(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	// Find the review items
-	doc.Find(".grid-cols-1 ").Each(func(i int, s *goquery.Selection) {
-		// For each item found, get the title
+	// Create an HTML string to store the parsed data
+	html := "<html><body>"
+
+	// Find the news items
+	doc.Find(".grid-cols-1").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the title and image URL
 		title := s.Find("a").Text()
-		fmt.Fprintf(w, "Review %d: %s\n", i, title)
+		imageURL, _ := s.Find("img").Attr("src")
+
+		// Append the title and image URL to the HTML string
+		html += fmt.Sprintf("<h2>%s</h2>", title)
+		html += fmt.Sprintf(`<img src="%s" alt="%s">`, imageURL, title)
 	})
+
+	html += "</body></html>"
+
+	// Set the response headers
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	// Write the HTML string to the response
+	w.Write([]byte(html))
 }
 
 func main() {
 	http.HandleFunc("/", ExampleScrape)
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
