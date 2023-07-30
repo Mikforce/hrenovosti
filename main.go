c036aff7636158317d2d3556d4eca0c8413e7bdd
Ivan <100131490+Mikforce@users.noreply.github.com>
2023-07-30 17:33:36 +0300

    добавил
    рандом новостей, и проверку записи повторных новостей

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"log"
+	"math/rand"
 	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
@@ -17,7 +18,54 @@ type News struct {
 	Link     string
 }
 
-func main() {
+func randomNewsHandler(w http.ResponseWriter, r *http.Request) {
+	// Устанавливаем соединение с базой данных
+	db, err := sql.Open("sqlite3", "news.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Вызываем функцию getRandomNews
+	result, err := getRandomNews(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Отправляем результат в шаблон
+	tmpl := template.Must(template.ParseFiles("html/home.html"))
+	err = tmpl.Execute(w, result)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func getRandomNews(db *sql.DB) (News, error) {
+	var id int
+	var title, imageUrl, link string
+
+	// Получаем количество записей в таблице
+	var count int
+	err := db.QueryRow("SELECT COUNT(*) FROM news").Scan(&count)
+	if err != nil {
+		return News{}, fmt.Errorf("ошибка при получении количества новостей: %v", err)
+	}
+
+	// Генерируем случайный индекс от 1 до count
+	randomIndex := rand.Intn(count) + 1
+
+	// Выполняем запрос на выборку рандомной записи
+	query := fmt.Sprintf("SELECT id, title, image_url, link FROM news WHERE id = %d", randomIndex)
+	err = db.QueryRow(query).Scan(&id, &title, &imageUrl, &link)
+	if err != nil {
+		return News{}, fmt.Errorf("ошибка при получении рандомной записи: %v", err)
+	}
+
+	// Формируем результат
+	result := News{Title: title, ImageURL: imageUrl, Link: "https://panorama.pub" + link}
+	return result, nil
+}
+
+func basa() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Определяем URL страницы, которую будем парсить
 		url := "https://panorama.pub/science"
@@ -51,19 +99,27 @@ func main() {
 		// 		link TEXT
 		// 	)
 		// `)
-		if err != nil {
-			log.Fatal(err)
-		}
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
 
 		newsList := []News{}
 
 		// Find the news items
 		doc.Find(".grid-cols-1").Each(func(i int, s *goquery.Selection) {
 			if i < 3 {
-				// For each item found, get the title, image URL, and link URL
+				// Для каждого найденного элемента получите заголовок, URL-адрес изображения и URL-адрес ссылки.
 				title := s.Find("a").Text()
 				imageURL, _ := s.Find("img").Attr("src")
 				link, _ := s.Find("a").Attr("href")
+
+				// Проверяем, существует ли уже запись с такими же данными
+				var count int
+				db.QueryRow("SELECT COUNT(*) FROM news WHERE title = ? AND image_url = ? AND link = ?", title, imageURL, link).Scan(&count)
+				if count > 0 {
+					// Запись уже существует, пропускаем ее
+					return
+				}
 
 				// Выводим данные новости
 				fmt.Println("Title:", title)
@@ -77,22 +133,32 @@ func main() {
 			`, title, imageURL, link)
 				if err != nil {
 					log.Fatal(err)
+
 				}
 
 				news := News{Title: title, ImageURL: imageURL, Link: "https://panorama.pub" + link}
 				newsList = append(newsList, news)
+
 			}
 		})
+
 		// Создаем шаблон HTML
 		tmpl := template.Must(template.ParseFiles("html/home.html"))
 
 		// Передаем список новостей в шаблон и генерируем HTML
+
 		err = tmpl.Execute(w, newsList)
 		if err != nil {
 			log.Fatal(err)
 		}
 	})
+}
+func main() {
 
-	fmt.Println("Server started on http://localhost:80")
-	log.Fatal(http.ListenAndServe(":80", nil))
+	http.HandleFunc("/", randomNewsHandler)
+	// Устанавливаем путь к файлам шаблонов
+	tmplPath := "html"
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(tmplPath))))
+	fmt.Println("Server started on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
