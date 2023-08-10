0090b73874d25bc8d76675a6a8b9d44a7dc1dad9
Ivan <100131490+Mikforce@users.noreply.github.com>
2023-08-10 16:58:24 +0300

    с фронтом от Шурика
    с фронтом от Шурика

--- main.go
+++ main.go
@@ -4,14 +4,11 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
 	"os/exec"
 	"strconv"
-	"time"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -57,75 +54,75 @@ func gopythontwo() {
 	}
 }
 
-func parseNews(url string) ([]News, error) {
-	// Отправляем GET-запрос и получаем содержимое страницы
-	response, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer response.Body.Close()
+// func parseNews(url string) ([]News, error) {
+// 	// Отправляем GET-запрос и получаем содержимое страницы
+// 	response, err := http.Get(url)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer response.Body.Close()
 
-	// Создаем объект goquery для парсинга HTML-кода
-	doc, err := goquery.NewDocumentFromReader(response.Body)
-	if err != nil {
-		return nil, err
-	}
+// 	// Создаем объект goquery для парсинга HTML-кода
+// 	doc, err := goquery.NewDocumentFromReader(response.Body)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	newsList := []News{}
+// 	newsList := []News{}
 
-	// Найти новости
-	doc.Find(".flex-col").Each(func(i int, s *goquery.Selection) {
-		if i < 3 {
-			// Для каждого найденного элемента получите заголовок, URL-адрес изображения и URL-адрес ссылки.
-			title := s.Find(".text-xl").Text()
-			imageURL, _ := s.Find("img").Attr("src")
-			link, _ := s.Find("a").Attr("href")
+// 	// Найти новости
+// 	doc.Find(".flex-col").Each(func(i int, s *goquery.Selection) {
+// 		if i < 3 {
+// 			// Для каждого найденного элемента получите заголовок, URL-адрес изображения и URL-адрес ссылки.
+// 			title := s.Find(".text-xl").Text()
+// 			imageURL, _ := s.Find("img").Attr("src")
+// 			link, _ := s.Find("a").Attr("href")
 
-			news := News{Title: title, ImageURL: imageURL, Link: link}
-			newsList = append(newsList, news)
-		}
-	})
+// 			news := News{Title: title, ImageURL: imageURL, Link: link}
+// 			newsList = append(newsList, news)
+// 		}
+// 	})
 
-	return newsList, nil
-}
+// 	return newsList, nil
+// }
 
-func baze() {
-	baseURL := "https://panorama.pub/science?page="
-	rand.Seed(time.Now().UnixNano())
-	numPages := rand.Intn(30) + 1
+// func baze() {
+// 	baseURL := "https://panorama.pub/science?page="
+// 	rand.Seed(time.Now().UnixNano())
+// 	numPages := rand.Intn(30) + 1
 
-	// Устанавливаем соединение с базой данных
-	db, err := sql.Open("sqlite3", "news.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+// 	// Устанавливаем соединение с базой данных
+// 	db, err := sql.Open("sqlite3", "news.db")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer db.Close()
 
-	// Создаем таблици вставляем данные
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS news ( 		id INTEGER PRIMARY KEY AUTOINCREMENT, 		title TEXT, 		image_url TEXT, 		link TEXT 	)")
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	// Создаем таблици вставляем данные
+// 	_, err = db.Exec("CREATE TABLE IF NOT EXISTS news ( 		id INTEGER PRIMARY KEY AUTOINCREMENT, 		title TEXT, 		image_url TEXT, 		link TEXT 	)")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	for i := 1; i <= numPages; i++ {
-		pageURL := fmt.Sprintf("%s%d", baseURL, i)
-		newsList, err := parseNews(pageURL)
-		if err != nil {
-			log.Println("Error parsing news:", err)
-			continue
-		}
+// 	for i := 1; i <= numPages; i++ {
+// 		pageURL := fmt.Sprintf("%s%d", baseURL, i)
+// 		newsList, err := parseNews(pageURL)
+// 		if err != nil {
+// 			log.Println("Error parsing news:", err)
+// 			continue
+// 		}
 
-		// Вставить данные в базу данных
-		for _, news := range newsList {
-			if news.Title != "" { // Проверяем, не является ли заголовок новости пустым
-				_, err = db.Exec("INSERT INTO news (title, image_url, link) VALUES (?, ?, ?)", news.Title, news.ImageURL, news.Link)
-				if err != nil {
-					log.Println("Error inserting data into database:", err)
-				}
-			}
-		}
-	}
-}
+//			// Вставить данные в базу данных
+//			for _, news := range newsList {
+//				if news.Title != "" { // Проверяем, не является ли заголовок новости пустым
+//					_, err = db.Exec("INSERT INTO news (title, image_url, link) VALUES (?, ?, ?)", news.Title, news.ImageURL, news.Link)
+//					if err != nil {
+//						log.Println("Error inserting data into database:", err)
+//					}
+//				}
+//			}
+//		}
+//	}
 func randomNewsHandler(c *gin.Context) {
 	// Устанавливаем соединение с базой данных
 	db, err := sql.Open("sqlite3", "news.db")
